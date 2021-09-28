033e4866ec9324cea296fd0a569162b4b2e67dbc
AshIsbitt <ashisbitt@icloud.com>
2021-09-28 15:12:05 +0100

    String attributes and methods]

--- CSharpCheatSheets/Program.cs
+++ CSharpCheatSheets/Program.cs
@@ -7,8 +7,16 @@ namespace CSharpCheatSheets
         static void Main(string[] args)
         {
         	//  Variable initialisation and inline strings (f-strings)
-        	sstring aFriend = "Liliana";
+        	string aFriend = "Liliana";
             Console.WriteLine($"Hello {aFriend}!");
+			// String attributes
+            Console.WriteLine($"{aFriend} is {aFriend.Length} letters long");
+			// String methods
+			string aFriend2 = "   Liliana   ";
+			Console.WriteLine($"{aFriend2.TrimStart()}");
+			Console.WriteLine($"{aFriend2.TrimEnd()}");
+			Console.WriteLine($"{aFriend2.Trim()}");
+            
         }
     }
 }
