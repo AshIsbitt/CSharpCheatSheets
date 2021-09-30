13e04eaa15f20bdb53d3becda361c20f28054ede
AshIsbitt <ashisbitt@icloud.com>
2021-09-30 16:10:06 +0100

    OOP via a bank program v1

---
+++ CSharpCheatSheets/Bank-OOP/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Bank_OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an object
+            var account = new BankAccount("Delilah", 1000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance}");
+            account.MakeWithdrawal(120, DateTime.Now, "Hammock");
+            Console.WriteLine(account.Balance);
+        }
+    }
+}
