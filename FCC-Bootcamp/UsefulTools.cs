651674ecc914944eae31604beb5101827179b8be
AshIsbitt <ashisbitt@icloud.com>
2021-10-04 18:34:42 +0100

    UsefulTools.cs from the static methods section:

--- FCC-Bootcamp/UsefulTools.cs
+++ FCC-Bootcamp/UsefulTools.cs
@@ -6,6 +6,7 @@ namespace FCC_Bootcamp
 {
     class UsefulTools
     {
+        // This is a static method
         public static void sayHi(string name)
         {
             Console.WriteLine($"Hello {name}!");
